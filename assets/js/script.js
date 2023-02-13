d176ff005ae7808b69589cc453d5e01a1160cfc0
unknown <jahandarmv@code.edu.az>
2023-02-14 00:33:22 +0400

    new task

---
+++ assets/js/script.js
@@ -0,0 +1,234 @@
+'use strict'
+// ------homework---------
+
+
+
+
+
+
+     // function name(text){
+    //     let reverseText=text.split("").reverse().join("")
+    //     console.log(reverseText)
+
+    // }
+    // name("caho")
+
+    // -----------------------
+
+
+
+
+
+
+
+
+
+
+
+
+// let address="Ehmedli"
+
+// console.log(address.length)
+
+// console.log(address[0])
+
+// console.log(address.charAt(0))
+
+// console.log(address.charCodeAt(1))
+
+// console.log(address.toUpperCase())
+
+// console.log(address.toLowerCase())
+
+// console.log(address.split(""))
+
+// console.log(address.trim())
+
+// console.log(address.trimEnd())
+
+// console.log(address.trimStart())
+
+// console.log(address.slice(1,3))
+
+// let text ="sdkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk"
+
+// if(text.length>10){
+//     console.log(text.substring(1,10) + "...")
+// }
+
+
+// const strToUpper= str =>{
+//     return str.charAt(0).toUpperCase()+str.slice(1)
+// }
+
+// console.log(strToUpper("hello"))
+
+    // let students=["Cavid","Jale","Roya","Konul","Cinare","Anar"]
+
+    // let text="R"
+
+    // function filter(arr,text){
+    //     let result=[];
+    //     for (const item of arr) {
+    //         if(item.toLowerCase().startsWith(text.toLowerCase())){
+    //             result.push(item)
+    //         }
+    //     }
+
+    //     return result;
+    // }
+
+
+    // console.log(filter(students,text))
+
+
+    // let students=["Cavid","Jale","Roya","Konul","Cinare","Anar"]
+
+    // let result=students.find(m=>m=="Jale")
+
+    // students.sort();
+
+    // console.log(students)
+
+
+    //  students.reverse();
+    // console.log(students)
+
+    // / students.reverse();
+
+    // console.log(students);
+    
+    // console.log([3, 8, -10, 23, 19, -4, -14, 27].sort((a,b)=>b-a));
+    
+    // students.fill("Eli")  
+    
+    // students.fill("Eli",0,2)  
+    // console.log(students);
+    
+    // console.log(students.every(m => m == "Chinare"));  
+    
+    
+    
+    // let arr = [10, 20, 30, 40, 5, 6, 7];
+    
+    // console.log(arr.every(m => m > 4));
+    
+    
+    // const fruits = ["banana","orange","apple","mango"];
+    // const f = fruits.entries();
+    
+    // for (const iterator of f) {
+    //     console.log(iterator);
+        
+    // }
+    
+    
+    // let arr = [ 10, 20, 30, 40, 5, 6, 7];
+    
+    
+    // console.log(arr.findIndex(m=>m>20));   
+    
+    
+    
+    // console.log(arr.includes(3));  
+    
+    
+    // console.log(Array.isArray(arr));   
+    
+    
+    // let text = "ABCDFE";
+    // const myArr =  Array.from(text);    
+    // console.log(myArr);
+    
+    
+    //console.log(arr.slice(2));  
+    
+    //  console.log(arr.slice(2,5)); 
+    
+    
+    
+    
+    // const fruits = ["Banana", "Orange", "Apple", "Mango"];
+    
+    // // fruits.splice(2, 0, "Lemon", "Kiwi");   
+    
+    // fruits.splice(2, 1, "Lemon", "Kiwi");    
+    // console.log(fruits);
+    
+    
+    // console.log(arr.toString());
+    
+    
+    
+    
+    // let age = "55";
+    
+    // let age2 = "33"
+    
+    // console.log(typeof age.toString());
+    
+    
+    // console.log(typeof Number(age));
+    
+    
+    // console.log(Number(age) + Number(age2));
+    
+    
+    // console.log(typeof parseInt(age));
+    
+    // function getNums(num1, ...arr) {
+    //     console.log(arr);
+    // }
+    
+    // getNums(1,2,3,4,5,6)
+    
+    
+    // let test = (...arr) => {
+    //     console.log(arr);
+    // }
+    
+    // test(1,2,3)
+    
+    
+    
+    // let arr1 = [1,2,3,4,5];
+    
+    
+    // let copyArr = [...arr1];
+    
+    // console.log(copyArr);
+    
+    // arr1[0] = 10;
+    
+    // console.log(arr1);
+    
+    
+    
+    // let user1 = {
+    
+    //     name: "Shaiq",
+    //     address: "Ehmedli"
+    // }
+    
+    // console.log(user1);
+    
+    
+    // let copyUser1 = {...user1}
+    
+    // console.log(copyUser1);
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
